8271fc618726ff34aafb0a04a9394597283fe004
xfhjly <xfhjly@126.com>
2024-04-29 18:02:34 +0800

    fix: config server

--- chat/src/main/java/cn/wildfire/chat/app/AppService.java
+++ chat/src/main/java/cn/wildfire/chat/app/AppService.java
@@ -54,8 +54,9 @@ public class AppService implements AppServiceProvider {
      * <br>
      * <br>
      */
-    public static String APP_SERVER_ADDRESS/*请仔细阅读上面的注释，http 前缀不能省略*/ = "http://wildfirechat.net:8888";
+//    public static String APP_SERVER_ADDRESS/*请仔细阅读上面的注释，http 前缀不能省略*/ = "http://wildfirechat.net:8888";
 //    public static String APP_SERVER_ADDRESS/*请仔细阅读上面的注释*/ = "https://app.wildfirechat.net";
+    public static String APP_SERVER_ADDRESS = "http://chat.dev.xmem.tech:8180";
 
     private AppService() {
 
